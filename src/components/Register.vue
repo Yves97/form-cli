6d296a402213f939408e8df3a8266f239e8ea6d5
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-02-27 21:56:04 +0000

    ending form

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,25 +2,25 @@
     <div class="main-card">
         <h3>{{message}}</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum mollitia inventore, a minus neque sequi quasi ullam iste facere maxime explicabo eligendi assumenda, molestias debitis consequatur tempora repellat commodi.</p>
-        <form>
+        <form @submit.prevent>
             <div class="input-group">
-                <input type="text" placeholder="Votre nom" class="input-name" v-model="post.firstname" @keyup="checkname">
+                <input type="text" placeholder="Votre nom" class="input-name" v-model="post.firstname" @keyup.stop="checkname">
                 <small></small>
             </div>
             <div class="input-group">
-                <input type="text" placeholder="Votre Prenom" class="input-lastname" v-model="post.lastname" @keyup="checkusername">
+                <input type="text" placeholder="Votre Prenom" class="input-lastname" v-model="post.lastname" @keyup.stop="checkusername">
                 <small></small>
             </div>
             <div class="input-group">
-                <input type="email" placeholder="Votre Email" class="input-email" v-model="post.email" @keyup="checkemail">
+                <input type="email" placeholder="Votre Email" class="input-email" v-model="post.email" @keyup.stop="checkemail">
                 <small></small>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Votre Mot de Passe" class="input-password" v-model="post.password" @keyup="checkpassword">
+                <input type="password" placeholder="Votre Mot de Passe" class="input-password" v-model="post.password" @keyup.stop="checkpassword">
                 <small></small>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Retapez votre mot de passe ici" class="input-password2" v-model="post.password2" @keyup="verifyPass">
+                <input type="password" placeholder="Retapez votre mot de passe ici" class="input-password2" v-model="post.password2" @keyup.stop="verifyPass">
                 <small></small>
             </div>
             <div>
@@ -39,7 +39,8 @@
                 <input type="checkbox" name="" id="agree" v-model="post.agree">
                 <label for="agree">J'accepte les termes des conditions pour l'inscription</label>
             </div>
-            <button class="send-btn">Envoyer</button>
+            <button class="send-btn" @click="send">Envoyer</button>
+            <p v-if="isSubmitted">Envoyé</p>
         </form>
     </div>
 </template>
@@ -48,6 +49,7 @@
 export default {
     data(){
         return {
+            isSubmitted: false,
             message : 'Register',
             post : {
                 firstname : '',
@@ -59,6 +61,7 @@ export default {
                 sexe: [],
                 agree: false,
                 re: new RegExp("[a-zA-Z0-9]"),
+                
             },
             formData:{
                 sexe: ['masculin','feminin'],
@@ -74,10 +77,11 @@ export default {
                 input.nextElementSibling.innerHTML = ''
             }
             else if(this.post.firstname.length <= 2 && test != null){
-                input.nextElementSibling.innerHTML = 'doit contenir plus de 5 chiffres ou/et lettres'
+                input.nextElementSibling.innerHTML = 'doit contenir plus de 2 chiffres ou/et lettres'
             }
             else{
                 input.nextElementSibling.innerHTML = ''
+                false
             }
         },
         checkusername(){
@@ -127,6 +131,9 @@ export default {
             else{
                 password2.nextElementSibling.innerHTML = ''
             }
+        },
+        send(){
+            this.isSubmitted = true
         }
     },
     filters:{
